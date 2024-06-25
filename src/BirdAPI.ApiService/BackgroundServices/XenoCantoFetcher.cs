eb8e3d9caaa1e4afd27b2052ff932fa065112ea0
LimitlessGreen <limitlessgreen@outlook.de>
2024-06-25 19:00:10 +0200

    Remove unused code

--- src/BirdAPI.ApiService/BackgroundServices/XenoCantoFetcher.cs
+++ src/BirdAPI.ApiService/BackgroundServices/XenoCantoFetcher.cs
@@ -5,9 +5,8 @@ using System.Text.Json;
 using System.Threading;
 using System.Threading.Tasks;
 using BirdAPI.ApiService.Commands;
-using BirdAPI.ApiService.Database;
-using BirdAPI.ApiService.Database.Models;
 using MediatR;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
 namespace BirdAPI.ApiService.BackgroundServices
@@ -15,24 +14,19 @@ namespace BirdAPI.ApiService.BackgroundServices
     public class XenoCantoFetcher : BackgroundService
     {
         private readonly IHttpClientFactory _httpClientFactory;
-        private readonly IHostApplicationLifetime _hostApplicationLifetime;
         private const string BaseUrl = "https://xeno-canto.org/api/2/recordings?query=q:";
         private const string ProgressFilePath = "fetchProgress.json";
         private readonly FetchProgress _progress;
-        private readonly IMediator _mediator;
         private readonly IServiceScopeFactory _serviceScopeFactory;
 
         public XenoCantoFetcher(
             IHttpClientFactory httpClientFactory, 
             IHostApplicationLifetime hostApplicationLifetime,
-            IMediator mediator,
             IServiceScopeFactory serviceScopeFactory)
         {
             _httpClientFactory = httpClientFactory;
-            _hostApplicationLifetime = hostApplicationLifetime;
             _progress = LoadProgress();
-            _hostApplicationLifetime.ApplicationStopping.Register(OnApplicationStopping);
-            _mediator = mediator;
+            hostApplicationLifetime.ApplicationStopping.Register(OnApplicationStopping);
             _serviceScopeFactory = serviceScopeFactory;
         }
 
@@ -138,15 +132,5 @@ namespace BirdAPI.ApiService.BackgroundServices
     {
         public char CurrentQuality { get; set; }
         public int CurrentPage { get; set; }
-    }
-
-    public class XenoCantoResponse
-    {
-        public string numRecordings { get; init; }
-        public string numSpecies { get; init; }
-        public int numPages { get; init; }
-        public int page { get; init; }
-        
-        public List<XenoCantoEntry> recordings { get; init; }
     }
 }
