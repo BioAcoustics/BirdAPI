845d73e531c156500042bd616b595076c87cefba
LimitlessGreen <limitlessgreen@outlook.de>
2024-07-03 20:31:28 +0200

    Add xc/{id} route

---
+++ src/BirdAPI.ApiService/Controllers/XenoCantoEntryController.cs
@@ -0,0 +1,19 @@
+﻿using System.Diagnostics;
+using System.Threading.Tasks;
+using BirdAPI.ApiService.Commands;
+using BirdAPI.ApiService.Database;
+using BirdAPI.ApiService.Database.Models;
+using Bogus;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BirdAPI.ApiService.Controllers;
+
+[ApiController]
+[Route("xc")]
+public class XenoCantoEntryController(IMediator mediator) : ControllerBase
+{
+    [HttpGet("{id}")]
+    public async Task<IActionResult> GetUserById(string id) => Ok(await mediator.Send(new GetXenoCantoItemByIdQuery { Id = id }));
+
+}
