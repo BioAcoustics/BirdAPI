5cff5fb2237787d8feaeea5087e7e89f94e1fd29
LimitlessGreen <limitlessgreen@outlook.de>
2024-07-22 17:11:40 +0200

    Add Tests

---
+++ src/BirdAPI.Tests/IntegrationTest1.cs
@@ -0,0 +1,23 @@
+using System.Net;
+
+namespace BirdAPI.Tests.Tests;
+
+public class IntegrationTest1
+{
+
+    [Fact]
+    public async Task GetWebResourceRootReturnsOkStatusCode()
+    {
+        // Arrange
+        var appHost = await DistributedApplicationTestingBuilder.CreateAsync<Projects.BirdAPI_AppHost>();
+        await using var app = await appHost.BuildAsync();
+        await app.StartAsync();
+
+        // Act
+        var httpClient = app.CreateHttpClient("webfrontend");
+        var response = await httpClient.GetAsync("/");
+
+        // Assert
+        Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+    }
+}
